0f544fd068a5f908840795c0a2eb3375396b743f
Knight2601 <knight2601@gmail.com>
2018-04-26 22:36:14 +0100

    Add project files.

---
+++ Informa.Agri.Pmd.Ai.Importer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Informa.Agri.Pmd.Ai.Importer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
